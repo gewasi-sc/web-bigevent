794646690411fca79768f24cf86b2c284d5aed8b
gewasi <sc18056098851@163.com>
2022-07-19 20:25:50 +0800

    完成了个人中心功能的开发

---
+++ assets/js/user/user_pwd.js
@@ -0,0 +1,41 @@
+$(function () {
+    var form = layui.form
+
+    // 增加表单的验证规则
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/
+            ,'密码必须6到12位，且不能出现空格'
+          ],
+        samPwd: function (value) {
+            if(value === $('[name = oldPwd]').val()) {
+                return '新旧密码不能一致'
+            }
+        },
+        rePwd: function (value) {
+                if(value !== $('[name = newPwd]').val()) {
+                    return '两次密码不一致'
+                }
+            }
+    })
+
+    // 表单的提交事件
+    $('.layui-form').on('submit', function (e) {
+        e.preventDefault()
+        
+        $.ajax({
+            method: 'POST',
+            url: '/my/updatepwd',
+            data: $(this).serialize(),
+            success: function (res) {
+                if(res.status !== 0) {
+                    return layui.layer.msg('更新密码失败')
+                }
+                layui.layer.msg('更新密码成功')
+                //  重置表单
+                $('.layui-form')[0].reset()
+            }
+        })
+    })
+
+})
